6eb2f78b9c61b7febdd05a2e7f58aa9b6e94e3cc
Tomoko Kawaguchi <tomo.k.dev@gmail.com>
2022-09-09 18:02:26 +1000

    Updates

---
+++ precache-manifest.a1594c2ff5423a21a93602f277f841de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca9d61346fb122baf5ae",
+    "url": "/robofriends/static/css/main.af68c236.chunk.css"
+  },
+  {
+    "revision": "ca9d61346fb122baf5ae",
+    "url": "/robofriends/static/js/main.ca9d6134.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "76e6c9cd7e7356b533b7",
+    "url": "/robofriends/static/css/2.f71f1581.chunk.css"
+  },
+  {
+    "revision": "76e6c9cd7e7356b533b7",
+    "url": "/robofriends/static/js/2.76e6c9cd.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "69b8489df9bab392846fc7dc93cd20b3",
+    "url": "/robofriends/index.html"
+  }
+];
